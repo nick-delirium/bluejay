5b115ad0c3338c90ae1802f4f3d3ba1897a863e2
nick-delirium <nick.delirium@pm.me>
2023-06-02 23:12:51 +0200

    fix: dates in tests

--- lib/utils/time.dart
+++ lib/utils/time.dart
@@ -3,5 +3,6 @@ import 'package:intl/intl.dart';
 String fullDateFromMs(int entryTs, bool isUtc) {
   DateTime timestamp =
       DateTime.fromMillisecondsSinceEpoch(entryTs * 1000, isUtc: isUtc);
-  return DateFormat('dd MMM, HH:mm').format(timestamp.toLocal());
+  return DateFormat('dd MMM, HH:mm')
+      .format(isUtc ? timestamp : timestamp.toLocal());
 }
--- test/time_utils_test.dart
+++ test/time_utils_test.dart
@@ -5,18 +5,18 @@ void main() {
   group('Full Date Formatting', () {
     test('Ensure correct date formatting', () {
       // Test case 1: Test with a specific timestamp
-      int timestamp = 1627584000; // July 29, 2021 20:40:00 gmt +1
-      String expectedDate = '29 Jul, 20:40';
+      int timestamp = 1627584000; // July 29, 2021 18:40:00 utc
+      String expectedDate = '29 Jul, 18:40';
       expect(fullDateFromMs(timestamp, true), expectedDate);
 
       // Test case 2: Test with a different timestamp
-      timestamp = 1660867200; // Aug 19, 2022 02:00:00 gmt +1
-      expectedDate = '19 Aug, 02:00';
+      timestamp = 1660867200; // Aug 19, 2022 00:00:00 utc
+      expectedDate = '19 Aug, 00:00';
       expect(fullDateFromMs(timestamp, true), expectedDate);
 
       // Test case 3: Test with a timestamp at a different time of the day
-      timestamp = 1672358400; // Dec 30, 2022 01:00:00 gmt +1
-      expectedDate = '30 Dec, 01:00';
+      timestamp = 1672358400; // Dec 30, 2022 00:00:00 utc
+      expectedDate = '30 Dec, 00:00';
       expect(fullDateFromMs(timestamp, true), expectedDate);
     });
   });
